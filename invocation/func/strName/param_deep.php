21079269ce0700a22142a57336af01f18b6625eb
YichaoXu <yichao.xu.19@ucl.ac.uk>
2023-07-03 14:59:59 -0400

    Debug Update on July 3: Passed and fixed the previous issues for the class related syntax

--- invocation/func/strName/param_deep.php
+++ invocation/func/strName/param_deep.php
@@ -1,7 +1,6 @@
 <?php
 function no_param(): void { echo "no_param\n"; }
-function no_param3($param): void { echo "no_param3\n"; $param(); }
-function no_param2($param): void { echo "no_param2\n"; no_param3($param); }
-function no_param1($param): void { echo "no_param1\n"; no_param2($param); }
+
+function no_param1($param): void { echo "no_param1\n"; $param(); }
 function single_param($param): void { echo "single_param \t"; no_param1($param);  }
 single_param("no_param");
